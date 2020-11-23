0b7302a56ba5f79e26d094ebea2b025c9d58e061
Graham Higgins <gjh@bel-epa.com>
2020-11-23 04:11:36 +0000

    keygen update

--- contrib/testgen/base58.py
+++ contrib/testgen/base58.py
@@ -4,8 +4,8 @@ Bitcoin base58 encoding and decoding.
 Based on https://bitcointalk.org/index.php?topic=1026.0 (public domain)
 '''
 import hashlib
+import binascii
 import fugue_hash
-print(hashlib.algorithms_available)
 
 # for compatibility with following code...
 class SHA256:
@@ -75,33 +75,55 @@ def b58decode(v, length = None):
 
 def checksum(v):
     """Return 32-bit checksum based on SHA256"""
-    return FUGUE.new(FUGUE.new(v).digest()).digest()[0:4]
+    # return SHA256.new(SHA256.new(v).digest()).digest()[0:4]
+    # res = binascii.hexlify(FUGUE.new(FUGUE.new(v))).decode('ascii')
+    # res = binascii.hexlify(FUGUE.new(FUGUE.new(v)))
+    res = FUGUE.new(FUGUE.new(v))
+    # print("checksum: ", res[0:4], type(res[0:4]))
+    print("returning checksum: ", res[0:4], type(res[0:4]), binascii.hexlify(res)[0:4])
+    # return res[0:4]
+    return binascii.hexlify(res)[0:4]
 
 def b58encode_chk(v):
     """b58encode a string, with 32-bit checksum"""
     return b58encode(v + checksum(v))
 
 def b58decode_chk(v):
+    import codecs
     """decode a base58 string, check and remove checksum"""
+    # print("v", v)
     result = b58decode(v)
+    # print("b58decode result", result)
     if result is None:
         return None
     h3 = checksum(result[:-4])
+    h4 = binascii.hexlify(result[-4:])
+    print("h3 (", h3, type(h3), ") - h4 (", h4, type(h4), ")")
+    h5 = h4.decode('ascii')
+    h6 = ''.join([
+        k for k in [h5[i:i+2] for i in range(0,len(h5)-1,2)]])
+    # if result[-4:] == h3:  #  checksum(result[:-4]):
     if result[-4:] == checksum(result[:-4]):
+        print("Returning result[:-4]")
         return result[:-4]
     else:
+        print("Returning None")
         return None
 
 def get_bcaddress_version(strAddress):
     """ Returns None if strAddress is invalid.  Otherwise returns integer version of address. """
+    # print("strAddress", strAddress)
     addr = b58decode_chk(strAddress)
+    print("addr", addr)
     if addr is None or len(addr)!=21: return None
     version = addr[0]
     return ord(version)
 
 if __name__ == '__main__':
     # Test case (from http://gitorious.org/bitcoin/python-base58.git)
-    assert get_bcaddress_version('FEyaPwV9xqRkCapaZSfJtrWhkhtyk9ohW1') is 0
+    version = get_bcaddress_version('FEyaPwV9xqRkCapaZSfJtrWhkhtyk9ohW1')
+    print("version", version)
+    assert version is 0
     _ohai = 'o hai'.encode('ascii')
     _tmp = b58encode(_ohai)
     assert _tmp == 'DYB3oMS'
